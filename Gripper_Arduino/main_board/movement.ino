ac4036a8eb660308c47c87921503bc3cb6563fbe
Sansei-CPP <67222723+Sansei-CPP@users.noreply.github.com>
2021-05-26 17:56:40 +0600

    Add files via upload
    Basic Functions seem to be ready

--- Gripper_Arduino/main_board/movement.ino
+++ Gripper_Arduino/main_board/movement.ino
@@ -1,11 +1,21 @@
+#define READY 0
+#define MOVE 1
+#define CALIB 2
+
 // Motor A connections
 int enA = 13;
 int in1 = 12;
 int in2 = 11;
+int lightCalib;
+int sensorValue;
 
-//int sensorPin = A0; // select the input pin for LDR
-//int sensorValue = 0;
-  
+int state = CALIB;
+
+//resistance drops, we can check changes in analogread
+int sensorPin = A0; // select the input pin for LDR
+
+int lightCal;
+
 int pstn = 0; // for incoming serial data
 int prev = 0;
 
@@ -28,23 +38,53 @@ void setup() {
   // Turn off motors - Initial state
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
+
+  pinMode(sensorPin, INPUT);
+  lightCal = analogRead(sensorPin);
 }
 
 void loop() {
-  while (Serial.available()==0){}             // wait for user input
-  pstn = Serial.parseInt(); 
-  Serial.print("I received: ");
-  Serial.println(pstn);
-  digitalWrite(pstn, HIGH);
-  digitalWrite(prev, LOW);
-  prev = pstn;
-  
-  directionControl();
-  delay(1000);
-//  sensorValue = analogRead(sensorPin); // read the value from the sensor
-//  Serial.println(sensorValue); //prints the values coming from the sensor on the screen
-//  delay(100);
-  
+  if(state == CALIB){
+    analogWrite(enA, 255);
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    delay(2000);
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    delay(2000);
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+    
+    lightCalib = analogRead(sensorPin);
+    Serial.println(sensorValue);
+    state = READY;    
+  }
+  if (Serial.available()>= 1){
+    pstn = Serial.parseInt(); 
+    Serial.print("I received: ");
+    Serial.println(pstn);
+    digitalWrite(pstn, HIGH);
+    digitalWrite(prev, LOW);
+    prev = pstn;
+    state = MOVE;
+    }             // wait for user input
+  if(state == MOVE){
+  sensorValue = analogRead(sensorPin); // read the value from the sensor
+  Serial.println(sensorValue); //prints the values coming from the sensor on the screen
+    if (sensorValue < lightCalib + 100){
+       directionControl();
+       state = READY;
+       Serial.println("found");
+  }
+  else if (sensorValue > lightCalib + 100) {
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+  }
+  }
+  else {
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);    
+  }
 }
 
 // This function lets you control spinning direction of motors
@@ -57,13 +97,4 @@ void directionControl() {
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
   delay(2000);
-  
-  // Now change motor directions
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
-  delay(2000);
-  
-  // Turn off motors
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
 }
