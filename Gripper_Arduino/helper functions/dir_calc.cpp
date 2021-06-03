0e8c02831f1ba01989b4ba4ddd2cf72a6e6c3d49
Asset Malik <asetmalik@gmail.com>
2021-06-03 13:07:18 +0600

    direction of finger

---
+++ Gripper_Arduino/helper functions/dir_calc.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <math.h>
+
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+bool is_next_on_right(int prev, int curr){
+	bool is_right = false;
+	
+	int delta = curr - prev;
+	if(delta > 0 && delta < 4) is_right = true;
+//	if(delta < 0 && delta > -4) is_right = false;
+	
+	if(delta < -4 && delta > -8) is_right = true;
+//	if(delta < 0 && delta > -4) is_right = false;
+	
+	return is_right;
+}
+
+
+void test(int prev, int curr, bool should){
+	string str = (is_next_on_right(prev, curr))?"right":"left ";
+	string st2 = (should)?"right":"left ";
+	
+	string st3 = (is_next_on_right(prev, curr) == should)?"✅":"❌";
+	
+	cout << prev << "->" << curr << " is [" << str << "], should be ["<< st2 << "]   "<< st3 <<  endl;
+}
+
+
+
+int main(int argc, char *argv[]) {
+	test(4,9,0);
+	test(9,4,1);
+	test(7,8,1);
+	test(6,5,0);
+	test(2,5,1);
+	
+	
+}
+
+
