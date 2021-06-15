b08291da9b0c9a414efd8855c069d2531d6698fd
Asset Malik <asetmalik@gmail.com>
2021-06-15 13:06:34 +0600

    roller Class

--- Gripper_Arduino/finger_board/Roller.cpp
+++ Gripper_Arduino/finger_board/Roller.cpp
@@ -2,9 +2,33 @@
 #include "Roller.h"
 
 void Roller::tick(){
+  sensorValue = analogRead(sensorPin);
+  
   switch(state){
-    case READY:{} break;
-    case MOVE:{} break;
-    case CALIB:{} break;
+    case READY:{
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, LOW);  
+    } break;
+    case MOVE:{
+      if (sensorValue < lightCalib + 200){
+         directionControl(is_next_on_right(prev, curr));
+      }
+      else if (sensorValue > lightCalib + 200) {
+         state = READY;
+//         Serial.println("found");
+      }  
+    } break;
+    case CALIB:{
+      lightCalib = analogRead(sensorPin);
+//      Serial.println(sensorValue);
+      state = READY;    
+    } break;
   }
+
+  if (x > -1){
+     state = MOVE;
+     prev = curr;
+     curr = x;
+     x = -1;
+  }  
 }
--- Gripper_Arduino/finger_board/Roller.h
+++ Gripper_Arduino/finger_board/Roller.h
@@ -10,7 +10,8 @@
 
 class Roller{
   public:
-    Roller(){
+    Roller(byte pinA, byte pinB, byte pinE, byte anal){
+      in1 = pinA; in2 = pinB; enA = pinE; sensorPin = anal;
       // Set all the motor control pins to outputs
       pinMode(enA, OUTPUT);
       pinMode(in1, OUTPUT);
@@ -22,17 +23,36 @@ class Roller{
     }
 
     void tick();
+
+    void setGoal(byte goal){
+      x = goal;
+    };
   private:
-    const byte enA = 13;
-    const byte in1 = 12;
-    const byte in2 = 11;
-    const byte sensorPin = A0;
+    byte enA,in1,in2,sensorPin;
 
     int lightCalib;
     int sensorValue;
-    int state = CALIB;
+    byte state = CALIB;
     int lightCal;
-    int x;
+    byte x;
+
+    byte prev = 4;
+    byte curr = 4;
+
+    void directionControl(bool toTheRight) {
+      analogWrite(enA, 255);
+      digitalWrite(in1, toTheRight);
+      digitalWrite(in2, !toTheRight);
+    }
+    
+    bool is_next_on_right(int prev, int curr){
+      bool is_right = false;
+      
+      int delta = curr - prev;
+      if(delta > 0 && delta < 4) is_right = true;
+      if(delta < -4 && delta > -8) is_right = true;
+      return is_right;
+    }
 };
 
 #endif
