a4a4712fc2a5e44f1237eef2f2c82b6ed8660c6d
Asset Malik <asetmalik@gmail.com>
2021-06-22 15:50:49 +0600

    Simple layout of finger list

--- GUI/go.mod
+++ GUI/go.mod
@@ -2,4 +2,8 @@ module main
 
 go 1.16
 
-require fyne.io/fyne/v2 v2.0.3
+require (
+	fyne.io/fyne v1.4.3 // indirect
+	fyne.io/fyne/v2 v2.0.3
+	github.com/ajstarks/fc v0.0.0-20210126164202-dcdd85f7e138 // indirect
+)
--- GUI/main.go
+++ GUI/main.go
@@ -1,22 +1,47 @@
 package main
 
 import (
+	"fmt"
+	"image/color"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
+func fingerList() fyne.CanvasObject {
+	var fingers []fyne.CanvasObject
+	for i := 0; i < 8; i++ {
+		fingers = append(fingers, fingerInfoItem(i))
+	}
+	return container.New(layout.NewVBoxLayout(), fingers...)
+}
+
+func fingerInfoItem(id int) fyne.CanvasObject {
+	name := widget.NewLabel(fmt.Sprintf("Finger %d Position:", id))
+	pos := widget.NewLabel(fmt.Sprintf("%d", id))
+	return container.New(layout.NewHBoxLayout(), name, pos)
+}
+
+func makeCircle() fyne.CanvasObject {
+	circle := canvas.NewCircle(color.Black)
+	circle.StrokeColor = color.Gray{0x99}
+	circle.StrokeWidth = 5
+	return circle
+}
+
+func mainLayout() fyne.CanvasObject {
+	return container.New(layout.NewHBoxLayout(), fingerList(), container.NewCenter(makeCircle()))
+}
+
 func main() {
-	a := app.New()
-	w := a.NewWindow("Hello")
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Box Layout")
+	myWindow.Resize(fyne.NewSize(400, 300))
 
-	hello := widget.NewLabel("Hello Fyne!")
-	w.SetContent(container.NewVBox(
-		hello,
-		widget.NewButton("Hi!", func() {
-			hello.SetText("Welcome :)")
-		}),
-	))
-
-	w.ShowAndRun()
+	myWindow.SetContent(mainLayout())
+	myWindow.ShowAndRun()
 }
